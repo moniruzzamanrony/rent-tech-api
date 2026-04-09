a4fcec0e9933ea92c29a68535a29327afea8cc14
Rony <eproni29@gmail.com>
2026-04-10 03:16:21 +0600

    Feat: Update conversion methods to support dynamic property inclusion and change collections to sets

--- src/main/java/com/itvillage/renttech/payment/VerificationDto.java
+++ src/main/java/com/itvillage/renttech/payment/VerificationDto.java
@@ -1,11 +1,14 @@
 package com.itvillage.renttech.payment;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class VerificationDto {
+    @JsonProperty("isSuccess")
     private boolean isSuccess;
-        private String message;
+
+    private String message;
 }
